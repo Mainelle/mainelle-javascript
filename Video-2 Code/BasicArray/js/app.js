e0e1871041132accba8d7aa6514abb1c004b6de6
Mainelle <80147231+Mainelle@users.noreply.github.com>
2021-03-06 10:46:45 +0200

    Add files via upload

---
+++ Video-2 Code/BasicArray/js/app.js
@@ -0,0 +1,7 @@
+
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below ths line
+var myArray = ["Quincy", 1];
---
+++ Video-2 Code/IneQuality Operator/js/app.js
@@ -0,0 +1,11 @@
+// Setup
+function testNotEqual(val) {
+    if (val !=99) {
+        return "Equal";
+    }
+    return "Not Equal"
+}
+
+
+Console.log(testNotEqual(99));
+
